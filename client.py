497eee4ff2ad31cd9282cb708fe66f278af49009
castolebron <71756489+castolebron@users.noreply.github.com>
2021-02-24 12:56:19 +0100

    thread

--- client.py
+++ client.py
@@ -1,60 +1,41 @@
-#!/usr/bin/env python3
-
-
-#input_string = 'Hello'
-#print(type(input_string))
-#input_bytes_encoded = input_string.encode()
-#print(type(input_bytes_encoded))
-#print(input_bytes_encoded)
-#output_string=input_bytes_encoded.decode()
-#print(type(output_string))
-#print(output_string)
-
 import socket
-import sys
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
+sock_service = socket.socket()
 
-#La funzione riceve la socket connessa al server e la utilizza per richiedere il servizio
-def invia_comandi(sock_service):
+def socket_connect (sock_service, SERVER_ADDRESS, SERVER_PORT):
+  
+    sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+    print("Connessione a: " + str((SERVER_ADDRESS, SERVER_PORT)))
+
+def input_data():
+
     while True:
-        try: #try per evitare vari possibili errori
-            dati = input("Inserisci i dati da inviare (ko per terminare la connessione): ")#inserimento dati in caso non trova errori
-        except EOFError: #se c'è un errore
-            print("\nOkay. Exit")# stampa questo 
-            break #chiusura
-        if not dati:# se non vengono insierriti dati 
-            print("Non puoi inviare una stringa vuota!")# meaasggio
-            continue# non chiude il ciclo ma lo continua comunque
-        if dati == 'ko':#se viene inserito ko
-            print("Chiudo la connessione con il server!")#il server si chiude
-            break#chiusura
-    
-        dati = dati.encode() #.encode trasforma la stringa in byte
-        sock_service.send(dati) #.send inivia a dati 
-        dati = sock_service.recv(2048) #.recv riceve i dati
-
-        if not dati:
-            print("Server non risponde. Exit")
+        try:
+            data = input("Inserisci operatore, numero uno e numero due da mandare: ")
+        except EOFError:
+            print("\nOkay. Esci")
+            break
+        if not data:
+            print("non puoi inviare una stringa vuota!")
+            continue
+        
+        if data == 'E' or data == 'e':
+            print("connessione col server conclusa!")
             break
         
-        dati = dati.decode()
-
-        print("Ricevuto dal server:")
-        print(dati + '\n')
-    sock_service.close()
+        data = data.encode()
+        sock_service.send(data)
+        data = sock_service.recv(2048)
+        if not data:
+            print("Il server non risponde")
+            break
         
-
-#la funzionme crea una socket(s) per la connessione con il server e la passa alla funzione invia_comandi(s)
-def connessione_server(address, port):
-    sock_service = socket.socket()
-    sock_service.connect((address, port))
-    print("Connesso a " + str((address, port)))
-    invia_comandi(sock_service)
-    
-
-#Questo comando serve per far capire al codice se è stato eseguito come singolo script o se è stato chiamato come modulo da qualche altro
-#programma per usare le sue funzioni o classi
-if __name__ == '__main__':
-    connessione_server(SERVER_ADDRESS, SERVER_PORT)
+        data = data.decode()
+        print("Ricevuto dal server:")
+        print(data + '\n')
+   
+socket_connect(sock_service, SERVER_ADDRESS, SERVER_PORT)
+input_data()
+sock_service.close()
--- server.py
+++ server.py
@@ -1,53 +1,73 @@
 #!/usr/bin/env python3
+#from -- import
 import socket
+from threading import Thread
 
-
-#è indifferente riempire questo campo o no
 SERVER_ADDRESS = '127.0.0.1'
-#numero porta, obbligatoriamente > 1024 perché le altre sono private
 SERVER_PORT = 22224
+sock_listen = socket.socket()
 
-def ricevi_comandi(sock_listen):
-    while True:
-        sock_service, addr_client = sock_listen.accept()
-        print("\nConnessione ricevuta da " + str(addr_client))
-        print("\nAspetto di ricevere i dati ")
-        contConn=0
-        while True:
-            dati = sock_service.recv(2048)
-            contConn+=1
-            if not dati:#se non riceve dati chiude la connessione
-                print("Fine dati dal client. Reset")
-                break
-            
-            dati = dati.decode()
-            print("Ricevuto: '%s'" % dati)
-            if dati=='ko':#se riceve ko chiude la connessione
-                print("Chiudo la connessione con " + str(addr_client))
-                break
-            operazione, primo, secondo = dati.split(";")#.split
-            #Vari if per selezionare l'operazione che il client ha inserito
-            if operazione == "piu":
-                risultato = int(primo) + int(secondo)
-            if operazione == "meno":
-                risultato = int(primo) - int(secondo)
-            if operazione == "per":
-                risultato = int(primo) * int(secondo)
-            if operazione == "diviso":
-                risultato = int(primo) / int(secondo)
-            
-            dati = "Il risultato dell'operazione: "+operazione +" tra "+primo+" e "+secondo+" è: "+str(risultato)#output
-            dati = dati.encode()
-            sock_service.send(dati)
-        sock_service.close()
-
-def avvia_server(address, port):
-    sock_listen = socket.socket()
+def socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT):#parte il  socket_listen
+    
     sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock_listen.bind((address, port))
-    sock_listen.listen(5)#consente un massimo di 5 client in coda
-    print("Server in ascolto su %s." % str((address, port)))
-    ricevi_comandi(sock_listen)
+    sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
+    sock_listen.listen(5)
+    print("Il server ascolta su: " + str((SERVER_ADDRESS, SERVER_PORT)))
 
-if __name__ == '__main__':
-    avvia_server(SERVER_ADDRESS, SERVER_PORT)
+def connected (addr_client):#parte la connessione
+    
+    print("\nConnesione ricevuta da " + str(addr_client))#stampo
+    print("\nCreazione di thread per gestire le richieste")
+    print("In attesa di ricevere dati ")
+
+def operation (sock_service, addr_client):
+   
+    while True:#iniza il  while
+        data = sock_service.recv(2048)
+        if not data:
+            print("fine data client.")
+            break
+        
+        data = data.decode()
+        print("\nRicevuto da " +  str(addr_client) + ": '%s'" %data)
+        if data=='0':
+            print("Chiusura connessione con : " + str(addr_client))
+            break
+        #end if
+        separator = data.split(';')
+        if separator[0] == "piu":
+            ris = (float(separator[1]) + float(separator[2]))
+        #end if
+        if separator[0] == "meno":
+            ris = (float(separator[1]) - float(separator[2]))
+        #end if
+        if separator[0] == "per":
+            ris = (float(separator[1]) * float(separator[2]))
+        #end if
+        if separator[0] == "diviso":
+            if separator[2] == '0':
+                ris = str(separator[1]) + " / " + str(separator[2]) +  "non è possibile"
+                data = "Risponde a : " + str(addr_client) + ".\n" + str(ris)
+            else:
+                ris = (float(separator[1]) / float(separator[2]))
+            
+       
+        data = "Rispondi a : " + str(addr_client) + ".\n il risulato tra  " + str(separator[1]) + " e " + str(separator[2]) + " con " + str(separator[0]) + " e: " + str(ris)
+        data = data.encode()
+        sock_service.send(data)
+    
+
+def receiving_connections(sock_listen):
+    
+    socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT)
+    while True:#inizi il  while
+        sock_service, addr_client = sock_listen.accept()
+        connected(addr_client)
+        try:
+            Thread(target = operation, args = (sock_service, addr_client)).start()
+        except:
+            print("The thread doesn't run")
+            sock_service.close()
+ 
+
+receiving_connections(sock_listen)
