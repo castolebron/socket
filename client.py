2d13460d8c03b1d5487a3ef9babd4f1ec8526611
castolebron <71756489+castolebron@users.noreply.github.com>
2021-03-02 12:00:14 +0100

    livello7

--- client.py
+++ client.py
@@ -1,41 +1,67 @@
-import socket
-
+#!/usr/bin/env python3
+#from -- import
+import socket, sys, random, os, time
+import threading, multiprocessing
+#variables
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
-sock_service = socket.socket()
-
-def socket_connect (sock_service, SERVER_ADDRESS, SERVER_PORT):
-  
-    sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
-    print("Connessione a: " + str((SERVER_ADDRESS, SERVER_PORT)))
-
-def input_data():
-
-    while True:
-        try:
-            data = input("Inserisci operatore, numero uno e numero due da mandare: ")
-        except EOFError:
-            print("\nOkay. Esci")
-            break
-        if not data:
-            print("non puoi inviare una stringa vuota!")
-            continue
-        
-        if data == 'E' or data == 'e':
-            print("connessione col server conclusa!")
-            break
-        
-        data = data.encode()
-        sock_service.send(data)
-        data = sock_service.recv(2048)
-        if not data:
-            print("Il server non risponde")
-            break
-        
-        data = data.decode()
-        print("Ricevuto dal server:")
-        print(data + '\n')
+NUM_WORKERS = 15
+def generate_requests (SERVER_ADDRESS, SERVER_PORT):#INIZIA  generate_requests
    
-socket_connect(sock_service, SERVER_ADDRESS, SERVER_PORT)
-input_data()
-sock_service.close()
+    start_time_thread = time.time()
+    print("Client PID: {pid}, Process Name: {process_name}, Thread Name: {thread_name}"
+            .format(pid = os.getpid(),
+            process_name = multiprocessing.current_process().name,#nome del processo
+            thread_name = threading.current_thread().name)#nome del thread
+    )
+    try:
+        sock_service = socket.socket()#creazione  client socket
+        sock_service.connect((SERVER_ADDRESS, SERVER_PORT))#connessione al server
+        print("{thread_name} Connecting to the Server: {server_address}:{server_port}"#stampo le info del server
+                .format(thread_name = threading.current_thread().name,
+                server_address = SERVER_ADDRESS,
+                server_port = SERVER_PORT)
+        )
+    except sock_service.error as socket_service_error:
+        print("{thread_name} Errore-> Error: {socket_service_error}"#stampo il messaggio di errore
+                .format(thread_name = threading.current_thread().name,#ottenego il nome del thread
+                socket_service_error = socket_service_error)#inserire l'errore del servizio socket
+
+        )
+        print("\nExiting...")
+        sys.exit()
+    
+    commands = ['piu', 'meno', 'per', 'diviso']
+    operation = commands[random.randint(0,3)]
+    data = str(operation) + ";" + str(random.randint(1,100)) + ";" + str(random.randint(1,100))
+    data = data.encode()#encoding data
+    sock_service.send(data)
+    data = sock_service.recv(2048)
+    if not data:
+        print("{thread_name}: Server doesn't response. Exiting".format(thread_name = threading.current_thread().name))
+   
+    data = data.decode()#decoding 
+    print("{thread_name} Received from the Server: " + data + "\n"
+            .format(thread_name = threading.current_thread().name)
+    )
+    data = "E"#carattere per chiudere 
+    data = data.encode()#encoding 
+    sock_service.send(data)#mando la data 
+    sock_service.close()
+    end_time_thread = time.time()
+    print("{thread_name} Execution time: {exe_time}"
+            .format(thread_name = threading.current_thread().name,
+            exe_time = end_time_thread - start_time_thread)
+    )
+
+if __name__ == '__main__':
+    
+    start_time = time.time()
+    processes = [multiprocessing.Process(target = generate_requests, args = (SERVER_ADDRESS, SERVER_PORT))for _ in range(NUM_WORKERS)]
+    [process.start() for process in processes]
+    [process.join() for process in processes]  
+    end_time = time.time()
+    print("{process_name} Execution time: {exe_time}"
+            .format(process_name = multiprocessing.current_process().name,
+            exe_time = end_time - start_time)
+    )
--- server.py
+++ server.py
@@ -7,67 +7,71 @@ SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
 sock_listen = socket.socket()
 
-def socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT):#parte il  socket_listen
+def socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT):
     
     sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
     sock_listen.listen(5)
-    print("Il server ascolta su: " + str((SERVER_ADDRESS, SERVER_PORT)))
+    print("Server listening on: %s" % str((SERVER_ADDRESS, SERVER_PORT)))#stampo informazioni server 
 
-def connected (addr_client):#parte la connessione
+def connected (addr_client):#inizia la connessione
     
-    print("\nConnesione ricevuta da " + str(addr_client))#stampo
-    print("\nCreazione di thread per gestire le richieste")
-    print("In attesa di ricevere dati ")
+    print("\nConnection received from " + str(addr_client))
+    print("\nCreating threads for manage the requests")
+    print("Waiting for receive data ")
 
 def operation (sock_service, addr_client):
-   
-    while True:#iniza il  while
+    
+    while True:
         data = sock_service.recv(2048)
-        if not data:
-            print("fine data client.")
-            break
-        
         data = data.decode()
-        print("\nRicevuto da " +  str(addr_client) + ": '%s'" %data)
-        if data=='0':
-            print("Chiusura connessione con : " + str(addr_client))
+        if not data:
+            print("End data client. Reset")
             break
-        #end if
+        elif data == "E":
+            print("Exiting...")
+            break
+       
+        print("\nReceived from " +  str(addr_client) + ": '%s'" %data)
+        if data=='0':
+            print("Closing connection with: " + str(addr_client))
+            break
+        #comincia la calcolatrice
         separator = data.split(';')
         if separator[0] == "piu":
             ris = (float(separator[1]) + float(separator[2]))
-        #end if
+       
         if separator[0] == "meno":
             ris = (float(separator[1]) - float(separator[2]))
-        #end if
+        
         if separator[0] == "per":
             ris = (float(separator[1]) * float(separator[2]))
-        #end if
+        
         if separator[0] == "diviso":
             if separator[2] == '0':
-                ris = str(separator[1]) + " / " + str(separator[2]) +  "non è possibile"
-                data = "Risponde a : " + str(addr_client) + ".\n" + str(ris)
+                ris = str(separator[1]) + " / " + str(separator[2]) +  " is not possible"
+                data = "Answer to: " + str(addr_client) + ".\n" + str(ris)
             else:
                 ris = (float(separator[1]) / float(separator[2]))
             
-       
-        data = "Rispondi a : " + str(addr_client) + ".\n il risulato tra  " + str(separator[1]) + " e " + str(separator[2]) + " con " + str(separator[0]) + " e: " + str(ris)
+        
+        data = "Answer to: " + str(addr_client) + ".\n The result between " + str(separator[1]) + " and " + str(separator[2]) + " with the " + str(separator[0]) + " is: " + str(ris)
         data = data.encode()
         sock_service.send(data)
     
+    sock_service.close()
 
 def receiving_connections(sock_listen):
     
     socket_listen(sock_listen, SERVER_ADDRESS, SERVER_PORT)
-    while True:#inizi il  while
+    while True:
         sock_service, addr_client = sock_listen.accept()
         connected(addr_client)
         try:
-            Thread(target = operation, args = (sock_service, addr_client)).start()
+            Thread(target = operation, args = (sock_service, addr_client)).start()#creazione thread
         except:
             print("The thread doesn't run")
             sock_service.close()
- 
+    
 
 receiving_connections(sock_listen)
